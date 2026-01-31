2fb8dc31873ff8a3333367ad02e091d273c33aaf
MD-ABDULLAH-KHAN <152681606+MD-ABDULLAH-KHAN@users.noreply.github.com>
2026-01-31 12:43:58 +0530

    Add files via upload

---
+++ Kanban Board/script.js
@@ -0,0 +1,119 @@
+let tasksData = {};
+
+const todo = document.querySelector("#todo");
+const progress = document.querySelector("#progress");
+const done = document.querySelector("#done");
+const columns = [todo, progress, done];
+
+const toggleModalButton = document.querySelector("#modal-toggle");
+const modal = document.querySelector(".modal");
+const modalBg = document.querySelector(".modal .bg");
+const addTaskButton = document.querySelector("#add-new-task");
+
+let dragElement = null;
+
+function addTask(title,desc,column){
+    const div = document.createElement("div");
+    div.classList.add("task");
+    div.setAttribute("draggable", "true")
+    div.innerHTML = `
+        <h2>${title}</h2>
+        <p>${desc}</p>
+        <button>Delete</button>
+    `;
+    column.appendChild(div);
+    div.addEventListener("drag", () => {
+        dragElement = div;
+    });
+    const deleteButton = div.querySelector("button");
+    deleteButton.addEventListener("click",()=>{
+        div.remove();
+        updateTaskCount();
+    })
+    return div;
+}
+
+function updateTaskCount(){
+    columns.forEach(col => {
+        const tasks = col.querySelectorAll(".task");
+        const count = col.querySelector(".right");
+        tasksData[col.id] = Array.from(tasks).map(t =>{
+            return {
+                title: t.querySelector("h2").innerText,
+                desc: t.querySelector("p").innerText
+            }
+        });
+        localStorage.setItem("tasks",JSON.stringify(tasksData));
+        count.innerText = tasks.length;
+    });
+}
+
+if(localStorage.getItem("tasks")){
+    const data = JSON.parse(localStorage.getItem("tasks"));
+
+    for(const col in data){
+        const column = document.querySelector(`#${col}`);
+        data[col].forEach(task=>{
+            addTask(task.title,task.desc,column);
+        })
+    }
+    updateTaskCount();
+}
+
+const tasks = document.querySelectorAll(".task");
+
+tasks.forEach(task => {
+    task.addEventListener("drag", () => {
+        dragElement = task;
+    });
+});
+
+function addDragEventsOnColumn(column) {
+    column.addEventListener("dragenter", e => {
+        e.preventDefault();
+        column.classList.add("hover-over");
+    });
+
+    column.addEventListener("dragleave", e => {
+        e.preventDefault();
+        column.classList.remove("hover-over");
+    });
+
+    column.addEventListener("dragover", e => {
+        e.preventDefault();
+    });
+
+    column.addEventListener("drop", e => {
+        e.preventDefault();
+        
+        column.appendChild(dragElement);
+        column.classList.remove("hover-over");
+
+        updateTaskCount();
+        
+    });
+}
+
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(progress);
+addDragEventsOnColumn(done);
+
+toggleModalButton.addEventListener("click", () => {
+    modal.classList.toggle("active");
+});
+
+modalBg.addEventListener("click", () => {
+    modal.classList.remove("active");
+});
+
+addTaskButton.addEventListener("click", () => {
+    const taskTitle = document.querySelector("#task-title-input").value;
+    const taskDesc = document.querySelector("#task-desc-input").value;
+
+    addTask(taskTitle,taskDesc,todo);
+    updateTaskCount();
+    modal.classList.remove("active");
+
+    document.querySelector("#task-title-input").value="";
+    document.querySelector("#task-desc-input").value="";
+});
